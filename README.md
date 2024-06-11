a9006e0dbf5da03975ac291f9e3d6d3bd8d55157
KiJeong-Lim <gijungdduk@naver.com>
2024-06-11 22:37:05 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# LoL : Logic of Logics
+
+- This repository is my proof pool.
+
+## start
+
+```
+eval `opam env`
+coq_makefile -f _CoqProject -o Makefile
+make -j4 -k
+```
+
+## coqc
+
+```
+The Coq Proof Assistant, version 8.18.0
+compiled with OCaml 5.1.1
+```
---
+++ theories/Prelude/Prelude.v
@@ -0,0 +1,13 @@
+Require Export Coq.Arith.Compare_dec.
+Require Export Coq.Arith.PeanoNat.
+Require Export Coq.Arith.Wf_nat.
+Require Export Coq.Bool.Bool.
+Require Export Coq.Classes.RelationClasses.
+Require Export Coq.Lists.List.
+Require Export Coq.micromega.Lia.
+Require Export Coq.Program.Basics.
+Require Export Coq.Program.Utils.
+Require Export Coq.Program.Wf.
+Require Export Coq.Relations.Relation_Definitions.
+Require Export Coq.Relations.Relation_Operators.
+Require Export Coq.Setoids.Setoid.
