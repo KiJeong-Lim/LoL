916d26847dbd07b4a0db184f76b3902f3ade3cc9
KiJeong-Lim <gijungdduk@naver.com>
2024-06-22 03:19:27 +0900

    + Class `isCountable`

--- theories/Prelude/Prelude.v
+++ theories/Prelude/Prelude.v
@@ -13,6 +13,16 @@ Require Export Coq.Relations.Relation_Operators.
 Require Export Coq.Setoids.Setoid.
 Require Export LoL.Prelude.SfLib.
 
+Notation " '⟪' x ':' t '⟫' " := (NW (fun x: unit => match x with tt => t end)) (x name, t at level 200, at level 0, no associativity) : type_scope.
+
+Definition Some_dec {A : Type} (x : option A)
+  : { x' : A | x = Some x' } + { x = None }.
+Proof.
+  destruct x as [x' | ].
+  - left. exists x'. reflexivity.
+  - right. reflexivity.
+Defined.
+
 #[universes(polymorphic)]
 Class cat@{u v} : Type :=
   { ob : Type@{u}
@@ -23,3 +33,110 @@ Class cat@{u v} : Type :=
 
 #[global] Coercion ob : cat >-> Sortclass.
 #[global] Coercion hom : cat >-> Funclass.
+
+Section EQ_DEC.
+
+Class hasEqDec (A : Type) : Type :=
+  eq_dec : forall x : A, forall y : A, {x = y} + {x <> y}.
+
+#[global]
+Instance Some_hasEqDec {A : Type} `{EQ_DEC : hasEqDec A} : hasEqDec (option A) :=
+  { eq_dec := ltac:(red in EQ_DEC; decide equality) }.
+
+End EQ_DEC.
+
+Section COUNTABLE. (* Reference: "https://plv.mpi-sws.org/coqdoc/stdpp/stdpp.countable.html" *)
+
+#[local] Obligation Tactic := idtac.
+
+Class isCountable (A : Type) : Type :=
+  { encode : A -> nat
+  ; decode : nat -> option A
+  ; decode_encode
+    : forall x, decode (encode x) = Some x 
+  }.
+
+Section SEARCH. (* Reference: "https://plv.mpi-sws.org/coqdoc/stdpp/stdpp.countable.html#choice" *)
+
+Context {A : Type} `{COUNTABLE : isCountable A}.
+
+Inductive search_step (P : A -> Prop) (n : nat) : nat -> Prop :=
+  | search_step_None
+    (NONE : decode n = None)
+    : search_step P n (S n)
+  | search_step_Some (x : A)
+    (NOT : ~ P x)
+    (SOME : decode n = Some x)
+    : search_step P n (S n).
+
+Lemma Acc_flip_search_step_P_0 (P : A -> Prop)
+  (BOUND : exists x : A, P x)
+  : Acc (flip (search_step P)) 0.
+Proof.
+  destruct BOUND as [x P_x]. enough (WTS : forall i, forall p, i <= encode x -> encode x = p + i -> Acc (flip (search_step P)) p) by now ii; eapply WTS with (i := encode x); lia.
+  induction i as [ | i IH]; simpl; intros p LE EQ.
+  - econs. intros j SEARCH. red in SEARCH. rewrite Nat.add_0_r in EQ. subst p. inv SEARCH.
+    + rewrite decode_encode in NONE. congruence.
+    + rewrite decode_encode in SOME. congruence.
+  - econs. intros j SEARCH. red in SEARCH. eapply IH.
+    + lia.
+    + inv SEARCH; lia.
+Qed.
+
+Fixpoint search_go (P : A -> Prop) (P_dec : forall x, {P x} + {~ P x}) (n : nat) (acc : Acc (flip (search_step P)) n) {struct acc} : A.
+Proof.
+  destruct (Some_dec (decode n)) as [[y SOME] | NONE].
+  - destruct (P_dec y) as [EQ | NE].
+    + exact y.
+    + exact (search_go P P_dec (S n) (Acc_inv acc (search_step_Some P n y NE SOME))).
+  - exact (search_go P P_dec (S n) (Acc_inv acc (search_step_None P n NONE))).
+Defined.
+
+Fixpoint search_go_spec (P : A -> Prop) (P_dec : forall x, {P x} + {~ P x}) (n : nat) (acc : Acc (flip (search_step P)) n) {struct acc}
+  : P (search_go P P_dec n acc).
+Proof.
+  destruct acc; simpl. destruct (Some_dec (decode n)) as [[? ?] | ?] eqn: ?.
+  - destruct (P_dec x).
+    + assumption.
+    + eapply search_go_spec.
+  - eapply search_go_spec.
+Qed.
+
+Lemma search_go_pirrel (P : A -> Prop) (P_dec : forall x, {P x} + {~ P x}) (n : nat) (acc : Acc (flip (search_step P)) n) (acc' : Acc (flip (search_step P)) n)
+  : search_go P P_dec n acc = search_go P P_dec n acc'.
+Proof.
+  revert acc acc acc'. intros acc''. induction acc'' as [? _ IH]; intros [?] [?]. simpl.
+  destruct (Some_dec (decode x)) as [[? ?] | ?] eqn: ?.
+  - destruct (P_dec x0) as [? | ?].
+    + reflexivity.
+    + eapply IH. eapply search_step_Some with (x := x0); trivial.
+  - eapply IH. eapply search_step_None; trivial.
+Qed.
+
+Definition search (n : nat) (BOUND : exists x : A, encode x = n) : A.
+Proof.
+  eapply search_go with (n := 0) (P := fun x : A => encode x = n).
+  - exact (fun x : A => Nat.eq_dec (encode x) n).
+  - eapply Acc_flip_search_step_P_0. exact BOUND.
+Defined.
+
+Theorem enumeration_lemma
+  (encode_surjective : forall n : nat, exists x : A, encode x = n)
+  : { enum : nat -> A & ⟪ enumerable : forall x : A, { n : nat | enum n = x } ⟫ }.
+Proof.
+  exists (fun n : nat => search n (encode_surjective n)). unnw. intros x. exists (encode x).
+  assert (claim : encode (search (encode x) (encode_surjective (encode x))) = encode x).
+  { eapply search_go_spec with (P := fun y => encode y = encode x) (P_dec := fun y => Nat.eq_dec (encode y) (encode x)). }
+  apply f_equal with (f := decode) in claim. do 2 rewrite decode_encode in claim. congruence.
+Qed.
+
+End SEARCH.
+
+#[local]
+Instance isCountable_if_enumerable {A : Type} (enum : nat -> A) (enumerable : forall x : A, { n : nat | enum n = x }) : isCountable A :=
+  { encode (x : A) := proj1_sig (enumerable x)
+  ; decode (n : nat) := Some (enum n)
+  ; decode_encode (x : A) := f_equal Some (proj2_sig (enumerable x))
+  }.
+
+End COUNTABLE.
