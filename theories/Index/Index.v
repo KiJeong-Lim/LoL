1cbaf85c5867176dfe3444ed577d18b644b411ae
KiJeong-Lim <gijungdduk@naver.com>
2024-06-21 23:00:11 +0900

    .

--- theories/Index/Index.v
+++ theories/Index/Index.v
@@ -1,4 +1,5 @@
 Require LoL.Data.Vector.
+Require LoL.Fol.Syntax.
 Require LoL.Math.Nat.
 Require LoL.Prelude.Prelude.
 Require LoL.Prelude.SfLib.
--- theories/Math/Nat.v
+++ theories/Math/Nat.v
@@ -39,13 +39,13 @@ Proof with lia || eauto.
 Qed.
 
 Lemma cpInv_rightInv (x : nat) (y : nat)
-  : (x, y) = cp (cpInv x y).
+  : cp (cpInv x y) = (x, y).
 Proof with lia || eauto.
   unfold cpInv. remember (x + y) as z eqn: z_eq. revert y x z_eq. induction z as [ | z IH].
   - simpl; ii. destruct x as [ | x'], y as [ | y']...
   - induction y as [ | y IHy]; ii.
     + rewrite z_eq. rewrite add_0_r with (n := x). rewrite add_0_r with (n := x) in z_eq. subst x.
-      rewrite add_0_r with (n := sum_from_0_to (S z)). simpl. rewrite <- add_comm. erewrite <- IH with (x := 0)...
+      rewrite add_0_r with (n := sum_from_0_to (S z)). simpl. rewrite <- add_comm. erewrite -> IH with (x := 0)...
     + assert (claim1 : z = x + y) by lia. subst z. clear z_eq. rename x into n, y into m. rewrite add_comm with (m := S m).
       assert (claim2 : S (n + m) = (S n) + m) by lia. apply IHy in claim2.
       simpl in *. rewrite add_comm. simpl. destruct (cp (n + m + sum_from_0_to (n + m) + m)) as [x y] eqn: H_OBS.
