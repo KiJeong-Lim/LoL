8b260c37872643bff059475f983c8c58e5d76cd3
KiJeong-Lim <gijungdduk@naver.com>
2024-06-24 20:47:43 +0900

    .

--- theories/Index/Index.v
+++ theories/Index/Index.v
@@ -1,5 +1,6 @@
 Require LoL.Data.Vector.
 Require LoL.Fol.Syntax.
+Require LoL.Logic.Computer.
 Require LoL.Math.ThN.
 Require LoL.Prelude.Notations.
 Require LoL.Prelude.Prelude.
---
+++ theories/Logic/Computer.v
@@ -0,0 +1,67 @@
+Require Import LoL.Prelude.Prelude.
+
+Fixpoint first_nat (p : nat -> bool) (n : nat) : nat :=
+  match n with
+  | O => 0
+  | S n' => if p (first_nat p n') then first_nat p n' else S n'
+  end.
+
+Theorem bounded_search (p : nat -> bool) (n : nat)
+  (WITNESS : p n = true)
+  (m := first_nat p n)
+  : p m = true /\ ⟪ MIN : forall i, p i = true -> i >= m ⟫.
+Proof with eauto.
+  unnw.
+  assert (claim1 : forall x, p x = true -> p (first_nat p x) = true).
+  { induction x as [ | x IH]... simpl. destruct (p (first_nat p x)) as [ | ] eqn: ?... }
+  split... intros i p_i_eq_true.
+  enough (claim2 : forall x, first_nat p x <= x).
+  enough (claim3 : forall x, p (first_nat p x) = true -> (forall y, x < y -> first_nat p x = first_nat p y)).
+  enough (claim4 : forall x, forall y, p y = true -> first_nat p x <= y)...
+  - intros x y p_y_eq_true. destruct (le_gt_dec x y) as [x_le_y | x_gt_y].
+    + eapply Nat.le_trans...
+    + replace (first_nat p x) with (first_nat p y)...
+  - intros x p_first_nat_p_x_eq_true y x_gt_y. induction x_gt_y as [ | y x_gt_y IH]; simpl.
+    + rewrite p_first_nat_p_x_eq_true...
+    + rewrite <- IH, p_first_nat_p_x_eq_true...
+  - induction x as [ | x IH]... simpl.
+    destruct (p (first_nat p x)) as [ | ]...
+Qed.
+
+Theorem unbounded_search (p : nat -> bool)
+  (EXISTENCE : exists n : nat, p n = true)
+  : { n : nat | p n = true }.
+Proof.
+  assert (COUNTABLE : isCountable nat).
+  { exists B.id Some. reflexivity. }
+  assert (P_dec : forall x : nat, {p x = true} + {p x <> true}).
+  { intros x. now destruct (p x) as [ | ]; [left | right]. }
+  pose proof (FUEL := @Acc_flip_search_step_P_0 nat COUNTABLE (fun x : nat => p x = true) EXISTENCE).
+  exists (@search_go nat COUNTABLE (fun x : nat => p x = true) P_dec 0 FUEL). eapply search_go_correct.
+Qed.
+
+(* [unary_mu]
+"""c
+int unary_mu(int (*f)(int n))
+{
+  int n;
+  for (n = 0; ; n++) {
+    if (f(n) == 0)
+      return n;
+  }
+}
+""" *)
+
+Theorem unary_mu (f : nat -> nat)
+  (EXISTENCE : exists n : nat, f n = 0)
+  : { n : nat | f n = 0 /\ (forall i, f i = 0 -> i >= n) }.
+Proof.
+  pose (p := fun n : nat => Nat.eqb (f n) 0).
+  assert (EXISTENCE' : exists n : nat, p n = true).
+  { destruct EXISTENCE as [n f_n_eq_0]. exists n. unfold p. rewrite Nat.eqb_eq. exact f_n_eq_0. }
+  pose proof (unbounded_search p EXISTENCE') as [n WITNESS].
+  exists (first_nat p n). unnw. pose proof (bounded_search p n WITNESS) as claim.
+  simpl in claim. unnw. destruct claim as [claim1 claim2]. split.
+  - rewrite <- Nat.eqb_eq with (n := f (first_nat p n)) (m := 0). exact claim1.
+  - intros i f_i_eq_0. eapply claim2. unfold p. rewrite Nat.eqb_eq. exact f_i_eq_0.
+Qed.
