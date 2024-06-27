6b3463f5b3a164423b262a31c0fa3c752760b566
KiJeong-Lim <gijungdduk@naver.com>
2024-06-27 11:07:24 +0900

    .

--- theories/Math/Topology.v
+++ theories/Math/Topology.v
@@ -3,19 +3,17 @@ Require Import LoL.Prelude.Prelude.
 #[local] Infix "\in" := E.In : type_scope.
 #[local] Infix "\subseteq" := E.isSubsetOf : type_scope.
 
-Class Topology (A : Type) : Type :=
+Class hasTopology (A : Type) : Type :=
   { isOpen (X : ensemble A) : Prop
-  ; isOpen_full
+  ; full_isOpen
     : isOpen E.full
-  ; isOpen_empty
-    : isOpen E.empty
-  ; isOpen_unions (Xs : ensemble (ensemble A))
-    (OPENs : forall X, X \in Xs -> isOpen X)
-    : isOpen (E.unions Xs)
-  ; isOpen_intersection (X1 : ensemble A) (X2 : ensemble A)
+  ; intersection_isOpen (X1 : ensemble A) (X2 : ensemble A)
     (OPEN1 : isOpen X1)
     (OPEN2 : isOpen X2)
     : isOpen (E.intersection X1 X2)
+  ; unions_isOpen (Xs : ensemble (ensemble A))
+    (OPENs : forall X, X \in Xs -> isOpen X)
+    : isOpen (E.unions Xs)
   ; isOpen_ext (X1 : ensemble A) (X2 : ensemble A)
     (OPEN : isOpen X1)
     (EXT_EQ : X1 == X2)
@@ -23,11 +21,19 @@ Class Topology (A : Type) : Type :=
   }.
 
 #[global]
-Add Parametric Morphism {A : Type} `{TOPOLOGY : Topology A}
+Add Parametric Morphism {A : Type} `{TOPOLOGY : hasTopology A}
   : (@isOpen A TOPOLOGY) with signature (eqProp ==> iff)
   as isOpen_compatWith_eqProp.
 Proof.
   intros X1 X2 EXT_EQ. split; intros OPEN.
   - eapply isOpen_ext with (X1 := X1). exact OPEN. exact EXT_EQ.
   - eapply isOpen_ext with (X1 := X2). exact OPEN. symmetry. exact EXT_EQ.
+Qed.
+
+Lemma empty_isOpen {A : Type} `{TOPOLOGY : hasTopology A}
+  : isOpen (@E.empty A).
+Proof.
+  assert (EQ : (@E.empty A) == E.unions E.empty).
+  { rewrite E.ensemble_eq_unfold. intros x. autorewrite with datatypes. now firstorder. }
+  rewrite EQ. eapply unions_isOpen. now firstorder.
 Qed.
