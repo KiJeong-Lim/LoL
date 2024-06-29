89f8cf505b3badf324c97a2829126161d47bd3e1
KiJeong-Lim <gijungdduk@naver.com>
2024-06-30 01:35:46 +0900

    .

--- theories/Logic/Computer.v
+++ theories/Logic/Computer.v
@@ -332,10 +332,10 @@ Proof.
   pose proof (LEFT := @PrimRecsGraph_complete). pose proof (RIGHT := @PrimRecsGraph_sound). now firstorder.
 Qed.
 
-Fixpoint PrimRecSpec_good (n : arity) (f : PrimRec n) (xs : Vector.t nat n) {struct f}
-  : forall z, eval_vec xs (runPrimRec f) = z -> PrimRecSpec n f xs z
-with PrimRecsSpec_good (n : arity) (m : arity) (fs : PrimRecs n m) (xs : Vector.t nat n) {struct fs}
-  : forall z, (forall i, eval_vec xs (runPrimRecs fs !! i) = z !! i) -> PrimRecsSpec n m fs xs z.
+Fixpoint PrimRecSpec_good (n : arity) (f : PrimRec n) {struct f}
+  : forall xs, forall z, eval_vec xs (runPrimRec f) = z -> PrimRecSpec n f xs z
+with PrimRecsSpec_good (n : arity) (m : arity) (fs : PrimRecs n m) {struct fs}
+  : forall xs, forall z, (forall i, eval_vec xs (runPrimRecs fs !! i) = z !! i) -> PrimRecsSpec n m fs xs z.
 Abort.
 
 End PRIMITIVE_RECURSION.
