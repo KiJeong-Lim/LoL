21425971fc876217145c9d8697ca95be84f95a91
KiJeong-Lim <gijungdduk@naver.com>
2024-06-30 14:36:57 +0900

    .

--- theories/Logic/Computer.v
+++ theories/Logic/Computer.v
@@ -2,6 +2,67 @@ Require Import LoL.Prelude.Prelude.
 Require Import LoL.Math.ThN.
 Require Import LoL.Data.Vector.
 
+Fixpoint first_nat (p : nat -> bool) (n : nat) : nat :=
+  match n with
+  | O => 0
+  | S n' => if p (first_nat p n') then first_nat p n' else n
+  end.
+
+Theorem first_nat_spec (p : nat -> bool) (n : nat)
+  (WITNESS : p n = true)
+  (m := first_nat p n)
+  : p m = true /\ ⟪ MIN : forall i, p i = true -> i >= m ⟫.
+Proof with eauto. (* This proof is assisted by "Clare Jang". *)
+  assert (claim1 : forall x, p x = true -> p (first_nat p x) = true).
+  { induction x as [ | x IH]... simpl. destruct (p (first_nat p x)) as [ | ] eqn: ?... }
+  unnw. split... intros i p_i_eq_true.
+  enough (claim2 : forall x, first_nat p x <= x).
+  enough (claim3 : forall x, p (first_nat p x) = true -> (forall y, x < y -> first_nat p x = first_nat p y)).
+  enough (claim4 : forall x, forall y, p y = true -> first_nat p x <= y)...
+  - intros x y p_y_eq_true. destruct (le_gt_dec x y) as [x_le_y | x_gt_y].
+    + eapply Nat.le_trans...
+    + replace (first_nat p x) with (first_nat p y)...
+  - intros x p_first_nat_p_x_eq_true y x_gt_y. induction x_gt_y as [ | y x_gt_y IH]; simpl.
+    + rewrite p_first_nat_p_x_eq_true...
+    + rewrite <- IH, p_first_nat_p_x_eq_true...
+  - induction x as [ | x IH]... simpl.
+    destruct (p (first_nat p x)) as [ | ]...
+Qed.
+
+Theorem nat_search_lemma (p : nat -> bool)
+  (EXISTENCE : exists n : nat, p n = true)
+  : { n : nat | p n = true }.
+Proof.
+  assert (COUNTABLE : isCountable nat).
+  { exists B.id Some. reflexivity. }
+  assert (P_dec : forall x : nat, {p x = true} + {p x <> true}).
+  { intros x. now destruct (p x) as [ | ]; [left | right]. }
+  pose proof (FUEL := @Acc_flip_search_step_P_0 nat COUNTABLE (fun x : nat => p x = true) EXISTENCE).
+  exists (@search_go nat COUNTABLE (fun x : nat => p x = true) P_dec 0 FUEL). eapply search_go_correct.
+Defined.
+
+Definition nullary_mu (f : nat -> nat)
+  (EXISTENCE : exists n : nat, f n = 0)
+  : { n : nat | f n = 0 /\ (forall i, f i = 0 -> i >= n) }.
+Proof.
+  pose (p := fun n : nat => Nat.eqb (f n) 0).
+  assert (EXISTENCE' : exists n : nat, p n = true).
+  { destruct EXISTENCE as [n f_n_eq_0]. exists n. unfold p. rewrite Nat.eqb_eq. exact f_n_eq_0. }
+  pose proof (nat_search_lemma p EXISTENCE') as [n WITNESS].
+  exists (first_nat p n). unnw. pose proof (first_nat_spec p n WITNESS) as claim.
+  simpl in claim. unnw. destruct claim as [claim1 claim2]. split.
+  - rewrite <- Nat.eqb_eq with (n := f (first_nat p n)) (m := 0). exact claim1.
+  - intros i f_i_eq_0. eapply claim2. unfold p. rewrite Nat.eqb_eq. exact f_i_eq_0.
+Defined.
+
+Example nullary_mu_example1
+  (f := fun n : nat => if Nat.ltb n 3 then 1 else 0)
+  (EXISTENCE := @ex_intro nat (fun n : nat => f n = 0) 5 eq_refl)
+  : proj1_sig (nullary_mu f EXISTENCE) = 3.
+Proof.
+  reflexivity.
+Qed.
+
 Section PRIMITIVE_RECURSION. (* Reference: "https://github.com/princeton-vl/CoqGym/blob/master/coq_projects/goedel/primRec.v" *)
 
 #[local] Open Scope program_scope.
@@ -138,43 +199,6 @@ Proof.
 Defined.
 *)
 
-Fixpoint runPrimRec {n : arity} (f : PrimRec n) {struct f} : naryFun n :=
-  match f with
-  | PR_succ => S
-  | PR_zero => O
-  | PR_proj n i => eval_proj (n := n) i
-  | PR_compose n m g h => eval_compose (n := n) (m := m) (runPrimRecs g) (runPrimRec h)
-  | PR_primRec n g h => eval_primRec (n := n) (runPrimRec g) (runPrimRec h)
-  end
-with runPrimRecs {n : arity} {m : arity} (fs : PrimRecs n m) {struct fs} : Vector.t (naryFun n) m :=
-  match fs with
-  | PRs_nil n' => VNil
-  | PRs_cons n' m' f fs' => VCons m' (runPrimRec f) (runPrimRecs fs')
-  end.
-
-Lemma runPrimRec_unfold (n : arity) (f : PrimRec n) :
-  runPrimRec f =
-  match f with
-  | PR_succ => S
-  | PR_zero => O
-  | PR_proj n i => eval_proj i
-  | PR_compose n m g h => eval_compose (runPrimRecs g) (runPrimRec h)
-  | PR_primRec n g h => eval_primRec (runPrimRec g) (runPrimRec h)
-  end.
-Proof.
-  destruct f; reflexivity.
-Defined.
-
-Lemma runPrimRecs_unfold (n : arity) (m : arity) (fs : PrimRecs n m) :
-  runPrimRecs fs =
-  match fs with
-  | PRs_nil n' => VNil
-  | PRs_cons n' m' f fs' => VCons m' (runPrimRec f) (runPrimRecs fs')
-  end.
-Proof.
-  destruct fs; reflexivity.
-Defined.
-
 Section PrimRecs_case.
 
 Let cast (x : nat) (n : nat) (m : nat) (EQ : n = m) : PrimRecs x n -> PrimRecs x m :=
@@ -222,6 +246,45 @@ Proof.
 Qed.
 
 End PrimRecs_case.
+
+Fixpoint runPrimRec {n : arity} (f : PrimRec n) {struct f} : naryFun n :=
+  match f with
+  | PR_succ => S
+  | PR_zero => O
+  | PR_proj n i => eval_proj (n := n) i
+  | PR_compose n m g h => eval_compose (n := n) (m := m) (runPrimRecs g) (runPrimRec h)
+  | PR_primRec n g h => eval_primRec (n := n) (runPrimRec g) (runPrimRec h)
+  end
+with runPrimRecs {n : arity} {m : arity} (fs : PrimRecs n m) {struct fs} : Vector.t (naryFun n) m :=
+  match fs with
+  | PRs_nil n' => VNil
+  | PRs_cons n' m' f fs' => VCons m' (runPrimRec f) (runPrimRecs fs')
+  end.
+
+Lemma runPrimRec_unfold (n : arity) (f : PrimRec n) :
+  runPrimRec f =
+  match f with
+  | PR_succ => S
+  | PR_zero => O
+  | PR_proj n i => eval_proj i
+  | PR_compose n m g h => eval_compose (runPrimRecs g) (runPrimRec h)
+  | PR_primRec n g h => eval_primRec (runPrimRec g) (runPrimRec h)
+  end.
+Proof.
+  destruct f; reflexivity.
+Defined.
+
+Lemma runPrimRecs_unfold (n : arity) (m : arity) (fs : PrimRecs n m) :
+  runPrimRecs fs =
+  match fs with
+  | PRs_nil n' => VNil
+  | PRs_cons n' m' f fs' => VCons m' (runPrimRec f) (runPrimRecs fs')
+  end.
+Proof.
+  destruct fs; reflexivity.
+Defined.
+
+(*
 
 #[local] Close Scope list_scope.
 #[local] Open Scope vector_scope.
@@ -338,68 +401,9 @@ with PrimRecsSpec_good (n : arity) (m : arity) (fs : PrimRecs n m) {struct fs}
   : forall xs, forall z, (forall i, eval_vec xs (runPrimRecs fs !! i) = z !! i) -> PrimRecsSpec n m fs xs z.
 Abort.
 
+*)
+
 End PRIMITIVE_RECURSION.
-
-Fixpoint first_nat (p : nat -> bool) (n : nat) : nat :=
-  match n with
-  | O => 0
-  | S n' => if p (first_nat p n') then first_nat p n' else n
-  end.
-
-Theorem first_nat_spec (p : nat -> bool) (n : nat)
-  (WITNESS : p n = true)
-  (m := first_nat p n)
-  : p m = true /\ ⟪ MIN : forall i, p i = true -> i >= m ⟫.
-Proof with eauto. (* This proof is assisted by "Clare Jang". *)
-  assert (claim1 : forall x, p x = true -> p (first_nat p x) = true).
-  { induction x as [ | x IH]... simpl. destruct (p (first_nat p x)) as [ | ] eqn: ?... }
-  unnw. split... intros i p_i_eq_true.
-  enough (claim2 : forall x, first_nat p x <= x).
-  enough (claim3 : forall x, p (first_nat p x) = true -> (forall y, x < y -> first_nat p x = first_nat p y)).
-  enough (claim4 : forall x, forall y, p y = true -> first_nat p x <= y)...
-  - intros x y p_y_eq_true. destruct (le_gt_dec x y) as [x_le_y | x_gt_y].
-    + eapply Nat.le_trans...
-    + replace (first_nat p x) with (first_nat p y)...
-  - intros x p_first_nat_p_x_eq_true y x_gt_y. induction x_gt_y as [ | y x_gt_y IH]; simpl.
-    + rewrite p_first_nat_p_x_eq_true...
-    + rewrite <- IH, p_first_nat_p_x_eq_true...
-  - induction x as [ | x IH]... simpl.
-    destruct (p (first_nat p x)) as [ | ]...
-Qed.
-
-Theorem nat_search_lemma (p : nat -> bool)
-  (EXISTENCE : exists n : nat, p n = true)
-  : { n : nat | p n = true }.
-Proof.
-  assert (COUNTABLE : isCountable nat).
-  { exists B.id Some. reflexivity. }
-  assert (P_dec : forall x : nat, {p x = true} + {p x <> true}).
-  { intros x. now destruct (p x) as [ | ]; [left | right]. }
-  pose proof (FUEL := @Acc_flip_search_step_P_0 nat COUNTABLE (fun x : nat => p x = true) EXISTENCE).
-  exists (@search_go nat COUNTABLE (fun x : nat => p x = true) P_dec 0 FUEL). eapply search_go_correct.
-Defined.
-
-Definition nullary_mu (f : nat -> nat)
-  (EXISTENCE : exists n : nat, f n = 0)
-  : { n : nat | f n = 0 /\ (forall i, f i = 0 -> i >= n) }.
-Proof.
-  pose (p := fun n : nat => Nat.eqb (f n) 0).
-  assert (EXISTENCE' : exists n : nat, p n = true).
-  { destruct EXISTENCE as [n f_n_eq_0]. exists n. unfold p. rewrite Nat.eqb_eq. exact f_n_eq_0. }
-  pose proof (nat_search_lemma p EXISTENCE') as [n WITNESS].
-  exists (first_nat p n). unnw. pose proof (first_nat_spec p n WITNESS) as claim.
-  simpl in claim. unnw. destruct claim as [claim1 claim2]. split.
-  - rewrite <- Nat.eqb_eq with (n := f (first_nat p n)) (m := 0). exact claim1.
-  - intros i f_i_eq_0. eapply claim2. unfold p. rewrite Nat.eqb_eq. exact f_i_eq_0.
-Defined.
-
-Example nullary_mu_example1
-  (f := fun n : nat => if Nat.ltb n 3 then 1 else 0)
-  (EXISTENCE := @ex_intro nat (fun n : nat => f n = 0) 5 eq_refl)
-  : proj1_sig (nullary_mu f EXISTENCE) = 3.
-Proof.
-  reflexivity.
-Qed.
 
 Section MU_RECURSIVE.
 
@@ -564,7 +568,7 @@ Proof.
         - exact ACC. 
       }
       subst y. eapply MuRec_isPartialFunction_aux.
-      * eapply SPEC2.
+      * exact SPEC2.
       * exact CALL.
     + simpl in CALL. destruct CALL as [MIN' CALL].
       assert (NOT_LT : ~ z < z').
